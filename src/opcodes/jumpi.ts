5b97632d251f165ccff2e1cd73004b7405ff4b7c
Sven <sven.larson.sw@tutanota.com>
2019-08-28 20:45:47 +0200

    Investigate jumpi opcode

--- src/opcodes/jumpi.ts
+++ src/opcodes/jumpi.ts
@@ -214,180 +214,199 @@ export default (opcode: Opcode, state: EVM): void => {
     const jumpLocation = state.stack.pop();
     const jumpCondition = state.stack.pop();
     const opcodes = state.getOpcodes();
-    console.log(jumpLocation);
-    console.log(state.conditions);
-    console.log('-');
     if (!BigNumber.isInstance(jumpLocation)) {
+        // target location is not a number
         state.halted = true;
         state.instructions.push(new JUMPI(jumpCondition, jumpLocation));
-    } else {
-        const jumpLocationData = opcodes.find((o: any) => o.pc === jumpLocation.toJSNumber());
-        if (!jumpLocationData || jumpLocationData.name !== 'JUMPDEST') {
-            //state.halted = true;
-            //state.instructions.push(new JUMPI(jumpCondition, jumpLocation));
-            state.instructions.push(new REQUIRE(jumpCondition));
-        } else if (BigNumber.isInstance(jumpCondition)) {
-            const jumpIndex = opcodes.indexOf(jumpLocationData);
-            if (
-                jumpIndex >= 0 &&
-                !jumpCondition.equals(0) &&
-                !(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps)
-            ) {
-                state.jumps[opcode.pc + ':' + jumpLocation.toJSNumber()] = true;
-                state.pc = jumpIndex;
-            }
-        } else if (
-            !(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps) &&
-            jumpCondition.name === 'SIG'
+        return;
+    }
+
+    const jumpLocationData = opcodes.find((o: any) => o.pc === jumpLocation.toJSNumber());
+    if (!jumpLocationData || jumpLocationData.name !== 'JUMPDEST') {
+        //state.halted = true;
+        //state.instructions.push(new JUMPI(jumpCondition, jumpLocation));
+
+        // target location doesn't exist or is not a JUMPDEST
+        state.instructions.push(new REQUIRE(jumpCondition));
+        return;
+    }
+    const jumpIndex = opcodes.indexOf(jumpLocationData);
+
+    if (BigNumber.isInstance(jumpCondition)) {
+        // the jump condition is a simple number
+        if (
+            jumpIndex >= 0 &&
+            !jumpCondition.equals(0) &&
+            !(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps)
         ) {
-            const jumpIndex = opcodes.indexOf(jumpLocationData);
-            if (jumpIndex >= 0) {
-                const functionClone: any = state.clone();
-                functionClone.pc = jumpIndex;
-                const functionCloneTree = functionClone.parse();
-                state.functions[jumpCondition.hash] = new TopLevelFunction(
-                    functionCloneTree,
-                    jumpCondition.hash,
-                    functionClone.gasUsed
-                );
+            // the jump condition is true, and we didn't jump before, so jump
+            state.jumps[opcode.pc + ':' + jumpLocation.toJSNumber()] = true;
+            state.pc = jumpIndex;
+        }
+    } else if (
+        !(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps) &&
+        jumpCondition.name === 'SIG'
+    ) {
+        // we didn't jump before and it's a SIG condition
+        console.log('JUMPI SIG');
+        if (jumpIndex >= 0) {
+            const functionClone: any = state.clone();
+            functionClone.pc = jumpIndex;
+            const functionCloneTree = functionClone.parse();
+            state.functions[jumpCondition.hash] = new TopLevelFunction(
+                functionCloneTree,
+                jumpCondition.hash,
+                functionClone.gasUsed
+            );
+            if (
+                jumpCondition.hash in functionHashes &&
+                functionCloneTree.length === 1 &&
+                functionCloneTree[0].name === 'RETURN' &&
+                functionCloneTree[0].items.every((item: any) => item.name === 'MappingLoad')
+            ) {
+                // function type 1; name is known
+                functionCloneTree[0].items.forEach((item: any) => {
+                    const fullFunction = (functionHashes as any)[jumpCondition.hash];
+                    state.mappings[item.location].name = fullFunction.split('(')[0];
+                    if (
+                        item.structlocation &&
+                        !state.mappings[item.location].structs.includes(item.structlocation)
+                    ) {
+                        state.mappings[item.location].structs.push(item.structlocation);
+                    }
+                });
+                delete state.functions[jumpCondition.hash];
+            } else if (
+                jumpCondition.hash in functionHashes &&
+                state.functions[jumpCondition.hash].items.length === 1 &&
+                state.functions[jumpCondition.hash].items[0].name === 'RETURN' &&
+                state.functions[jumpCondition.hash].items[0].items.length === 1 &&
+                state.functions[jumpCondition.hash].items[0].items[0].name === 'SLOAD' &&
+                BigNumber.isInstance(state.functions[jumpCondition.hash].items[0].items[0].location)
+            ) {
+                // function type 2
                 if (
-                    jumpCondition.hash in functionHashes &&
-                    functionCloneTree.length === 1 &&
-                    functionCloneTree[0].name === 'RETURN' &&
-                    functionCloneTree[0].items.every((item: any) => item.name === 'MappingLoad')
-                ) {
-                    functionCloneTree[0].items.forEach((item: any) => {
-                        const fullFunction = (functionHashes as any)[jumpCondition.hash];
-                        state.mappings[item.location].name = fullFunction.split('(')[0];
-                        if (
-                            item.structlocation &&
-                            !state.mappings[item.location].structs.includes(item.structlocation)
-                        ) {
-                            state.mappings[item.location].structs.push(item.structlocation);
-                        }
-                    });
-                    delete state.functions[jumpCondition.hash];
-                } else if (
-                    jumpCondition.hash in functionHashes &&
-                    state.functions[jumpCondition.hash].items.length === 1 &&
-                    state.functions[jumpCondition.hash].items[0].name === 'RETURN' &&
-                    state.functions[jumpCondition.hash].items[0].items.length === 1 &&
-                    state.functions[jumpCondition.hash].items[0].items[0].name === 'SLOAD' &&
-                    BigNumber.isInstance(
-                        state.functions[jumpCondition.hash].items[0].items[0].location
+                    !(
+                        state.functions[jumpCondition.hash].items[0].items[0].location in
+                        state.variables
                     )
                 ) {
-                    if (
-                        !(
-                            state.functions[jumpCondition.hash].items[0].items[0].location in
-                            state.variables
-                        )
-                    ) {
-                        const fullFunction = (functionHashes as any)[jumpCondition.hash];
-                        state.variables[
-                            state.functions[jumpCondition.hash].items[0].items[0].location
-                        ] = new Variable(fullFunction.split('(')[0], []);
-                        delete state.functions[jumpCondition.hash];
-                    } else {
-                        const fullFunction = (functionHashes as any)[jumpCondition.hash];
-                        state.variables[
-                            state.functions[jumpCondition.hash].items[0].items[0].location
-                        ].label = fullFunction.split('(')[0];
-                        delete state.functions[jumpCondition.hash];
-                    }
-                }
-            }
-        } else if (
-            !(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps) &&
-            ((jumpCondition.name === 'LT' &&
-                jumpCondition.left.name === 'CALLDATASIZE' &&
-                BigNumber.isInstance(jumpCondition.right) &&
-                jumpCondition.right.equals(4)) ||
-                (jumpCondition.name === 'ISZERO' && jumpCondition.item.name === 'CALLDATASIZE'))
-        ) {
-            const jumpIndex = opcodes.indexOf(jumpLocationData);
-            if (jumpIndex >= 0) {
-                state.halted = true;
-                const trueClone: any = state.clone();
-                trueClone.pc = jumpIndex;
-                trueClone.conditions.push(jumpCondition);
-                const trueCloneTree = trueClone.parse();
-                const falseClone = state.clone();
-                falseClone.pc = state.pc + 1;
-                const falseCloneTree: any = falseClone.parse();
-                if (
-                    trueCloneTree.length > 0 &&
-                    trueCloneTree.length === falseCloneTree.length &&
-                    trueCloneTree[0].name !== 'REVERT' &&
-                    trueCloneTree[0].name !== 'INVALID' &&
-                    trueCloneTree.map((item: any) => stringify(item)).join('') ===
-                        falseCloneTree.map((item: any) => stringify(item)).join('')
-                ) {
-                    state.functions[''] = new TopLevelFunction(
-                        trueCloneTree,
-                        '',
-                        trueCloneTree.gasUsed
-                    );
-                } else if (
-                    trueCloneTree.length > 0 &&
-                    trueCloneTree[0].name !== 'REVERT' &&
-                    trueCloneTree[0].name !== 'INVALID'
-                ) {
-                    state.instructions.push(
-                        new JUMPI(jumpCondition, jumpLocation, trueCloneTree, falseCloneTree)
-                    );
-                }
-            } else {
-                state.instructions.push(new JUMPI(jumpCondition, jumpLocation));
-            }
-        } else if (!(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps)) {
-            const jumpIndex = opcodes.indexOf(jumpLocationData);
-            state.jumps[opcode.pc + ':' + jumpLocation.toJSNumber()] = true;
-            if (jumpIndex >= 0) {
-                state.halted = true;
-                const trueClone: any = state.clone();
-                trueClone.pc = jumpIndex;
-                trueClone.conditions.push(jumpCondition);
-                const trueCloneTree = trueClone.parse();
-                const falseClone = state.clone();
-                falseClone.pc = state.pc + 1;
-                const falseCloneTree: any = falseClone.parse();
-                if (
-                    (falseCloneTree.length === 1 &&
-                        'name' in falseCloneTree[0] &&
-                        (falseCloneTree[0].name === 'REVERT' &&
-                            falseCloneTree[0].items &&
-                            falseCloneTree[0].items.length === 0)) ||
-                    falseCloneTree[0].name === 'INVALID'
-                ) {
-                    if (
-                        jumpCondition.name === 'CALL' &&
-                        BigNumber.isInstance(jumpCondition.memoryLength) &&
-                        jumpCondition.memoryLength.isZero() &&
-                        BigNumber.isInstance(jumpCondition.outputLength) &&
-                        jumpCondition.outputLength.isZero() &&
-                        jumpCondition.gas.name === 'MUL' &&
-                        jumpCondition.gas.left.name === 'ISZERO' &&
-                        BigNumber.isInstance(jumpCondition.gas.right) &&
-                        jumpCondition.gas.right.equals(2300)
-                    ) {
-                        jumpCondition.throwOnFail = true;
-                        state.instructions.push(jumpCondition);
-                        state.instructions.push(...trueCloneTree);
-                    } else {
-                        state.instructions.push(new REQUIRE(jumpCondition));
-                        state.instructions.push(...trueCloneTree);
-                    }
+                    // function type 2.1 : new variable
+                    const fullFunction = (functionHashes as any)[jumpCondition.hash];
+                    state.variables[
+                        state.functions[jumpCondition.hash].items[0].items[0].location
+                    ] = new Variable(fullFunction.split('(')[0], []);
+                    delete state.functions[jumpCondition.hash];
                 } else {
-                    state.instructions.push(
-                        new JUMPI(jumpCondition, jumpLocation, trueCloneTree, falseCloneTree)
-                    );
+                    // function type 2.2 : known variable
+                    const fullFunction = (functionHashes as any)[jumpCondition.hash];
+                    state.variables[
+                        state.functions[jumpCondition.hash].items[0].items[0].location
+                    ].label = fullFunction.split('(')[0];
+                    delete state.functions[jumpCondition.hash];
                 }
-            } else {
-                state.instructions.push(new JUMPI(jumpCondition, jumpLocation));
+            }
+        }
+    } else if (
+        !(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps) &&
+        ((jumpCondition.name === 'LT' &&
+            jumpCondition.left.name === 'CALLDATASIZE' &&
+            BigNumber.isInstance(jumpCondition.right) &&
+            jumpCondition.right.equals(4)) ||
+            (jumpCondition.name === 'ISZERO' && jumpCondition.item.name === 'CALLDATASIZE'))
+    ) {
+        console.log('JUMPI CALLDATASIZE');
+        // we didn't jumpb before, and the condition is a
+        // CALLDATASIZE < 4
+        // CALLDATASIZE == 0
+
+        // this happens really at the start of a contract; the
+        // JUMPI goes to the fallback function
+        if (jumpIndex >= 0) {
+            state.halted = true;
+            const trueClone: any = state.clone();
+            trueClone.pc = jumpIndex;
+            trueClone.conditions.push(jumpCondition);
+            const trueCloneTree = trueClone.parse();
+            const falseClone = state.clone();
+            falseClone.pc = state.pc + 1;
+            const falseCloneTree: any = falseClone.parse();
+            if (
+                trueCloneTree.length > 0 &&
+                trueCloneTree.length === falseCloneTree.length &&
+                trueCloneTree[0].name !== 'REVERT' &&
+                trueCloneTree[0].name !== 'INVALID' &&
+                trueCloneTree.map((item: any) => stringify(item)).join('') ===
+                    falseCloneTree.map((item: any) => stringify(item)).join('')
+            ) {
+                state.functions[''] = new TopLevelFunction(
+                    trueCloneTree,
+                    '',
+                    trueCloneTree.gasUsed
+                );
+            } else if (
+                trueCloneTree.length > 0 &&
+                trueCloneTree[0].name !== 'REVERT' &&
+                trueCloneTree[0].name !== 'INVALID'
+            ) {
+                state.instructions.push(
+                    new JUMPI(jumpCondition, jumpLocation, trueCloneTree, falseCloneTree)
+                );
             }
         } else {
-            state.instructions.push(new JUMPI(jumpCondition, jumpLocation, null, null, true));
+            state.instructions.push(new JUMPI(jumpCondition, jumpLocation));
         }
+    } else if (!(opcode.pc + ':' + jumpLocation.toJSNumber() in state.jumps)) {
+        // we didn't jump before
+        console.log('REGULAR JUMPI');
+
+        state.jumps[opcode.pc + ':' + jumpLocation.toJSNumber()] = true;
+        if (jumpIndex >= 0) {
+            state.halted = true;
+            const trueClone: any = state.clone();
+            trueClone.pc = jumpIndex;
+            trueClone.conditions.push(jumpCondition);
+            const trueCloneTree = trueClone.parse();
+            const falseClone = state.clone();
+            falseClone.pc = state.pc + 1;
+            const falseCloneTree: any = falseClone.parse();
+            if (
+                (falseCloneTree.length === 1 &&
+                    'name' in falseCloneTree[0] &&
+                    (falseCloneTree[0].name === 'REVERT' &&
+                        falseCloneTree[0].items &&
+                        falseCloneTree[0].items.length === 0)) ||
+                falseCloneTree[0].name === 'INVALID'
+            ) {
+                if (
+                    jumpCondition.name === 'CALL' &&
+                    BigNumber.isInstance(jumpCondition.memoryLength) &&
+                    jumpCondition.memoryLength.isZero() &&
+                    BigNumber.isInstance(jumpCondition.outputLength) &&
+                    jumpCondition.outputLength.isZero() &&
+                    jumpCondition.gas.name === 'MUL' &&
+                    jumpCondition.gas.left.name === 'ISZERO' &&
+                    BigNumber.isInstance(jumpCondition.gas.right) &&
+                    jumpCondition.gas.right.equals(2300)
+                ) {
+                    jumpCondition.throwOnFail = true;
+                    state.instructions.push(jumpCondition);
+                    state.instructions.push(...trueCloneTree);
+                } else {
+                    state.instructions.push(new REQUIRE(jumpCondition));
+                    state.instructions.push(...trueCloneTree);
+                }
+            } else {
+                state.instructions.push(
+                    new JUMPI(jumpCondition, jumpLocation, trueCloneTree, falseCloneTree)
+                );
+            }
+        } else {
+            // invalid version?
+            state.instructions.push(new JUMPI(jumpCondition, jumpLocation));
+        }
+    } else {
+        // skipped version
+        state.instructions.push(new JUMPI(jumpCondition, jumpLocation, null, null, true));
     }
 };
